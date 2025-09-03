71a961747bec1d7f1b6664bc6aed3f6f992054f2
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-04 01:03:36 +0200

    [UPD] inizio bonus

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   // variabile per selezionare il tipo di genere
   const [genre, setGenre] = useState("");
   const [movies, setMovies] = useState(initialsMovies);
+  const [search, setSearch] = useState("");
 
   // useEffect per filtrare nell'array
   useEffect(() => {
@@ -33,21 +34,33 @@ function App() {
           <div className="col-12">
             <h1>Movies Filter</h1>
           </div>
-
           <div className="col-12">
-            <select
-              name=""
-              id=""
-              className="form-select my-3"
-              value={genre}
-              onChange={(e) => setGenre(e.target.value)}
-            >
-              <option value="">Seleziona genere</option>
-              <option value="Fantascienza">Fantascienza</option>
-              <option value="Thriller">Thriller</option>
-              <option value="Romantico">Romantico</option>
-              <option value="Azione">Azione</option>
-            </select>
+            <div className="d-flex">
+              <div className="w-50 me-2">
+                <select
+                  name=""
+                  id=""
+                  className="form-select my-3"
+                  value={genre}
+                  onChange={(e) => setGenre(e.target.value)}
+                >
+                  <option value="">Seleziona genere</option>
+                  <option value="Fantascienza">Fantascienza</option>
+                  <option value="Thriller">Thriller</option>
+                  <option value="Romantico">Romantico</option>
+                  <option value="Azione">Azione</option>
+                </select>
+              </div>
+              <div className="w-50">
+                <input
+                  type="text"
+                  className="form-control my-3"
+                  placeholder="Filtra per nome"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+              </div>
+            </div>
 
             <ul className="list-group ms-0">
               {movies.map((movie, index) => {
